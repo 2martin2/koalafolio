c3e42033258ab218d7dd39a9ae356bfb7a3b533c
martin <vollertmartin@gmail.com>
2019-03-13 20:10:57 +0100

    version ++

--- PcpCore/settings.py
+++ PcpCore/settings.py
@@ -36,7 +36,7 @@ class Settings(configparser.ConfigParser):
         # set default settings
         # general settings
         self['general'] = {}
-        self['general']['version'] = '0.5.0'
+        self['general']['version'] = '0.5.1'
         self['general']['timeModeDaywise'] = 'True'
         self['general']['priceUpdateInterval(s)'] = '100'
         # proxy settings
@@ -71,7 +71,7 @@ class Settings(configparser.ConfigParser):
     def readSettings(self):
         try:
             self.read(self.filePath)
-            self['general']['version'] = '0.5.0'
+            self['general']['version'] = '0.5.1'
             logger.globalLogger.info('settings loaded')
         except Exception as ex:
             logger.globalLogger.error('settings can not be loaded: ' + str(ex))
