377d4bd2e4ad5d1e7f91e459e7ed49419c0b5443
martin <vollertmartin@gmail.com>
2020-11-25 21:50:59 +0100

    load last 5 years from kraken api

--- koalafolio/web/krakenApi.py
+++ koalafolio/web/krakenApi.py
@@ -7,6 +7,7 @@ Created on Sun Sep  15 15:15:19 2019
 import pandas as pd
 import krakenex
 import pykrakenapi
+import datetime
 
 # help(pykrakenapi.KrakenAPI)
 
@@ -17,8 +18,19 @@ def initApi(key, secret):
 
 def getTradeHistory(key, secret):
     k = initApi(key, secret)
-    tradeHistory = k.get_trades_history()[0]
-    newTradeHistory = tradeHistory.reset_index()
+    today = datetime.datetime.now()
+    trades = []
+    # get last 5 years
+    for year in range(today.year-5, today.year):
+        tradeHistory = k.get_trades_history(start=datetime.datetime(year=year, month=1, day=1).timestamp(),
+                                            end=datetime.datetime(year=year+1, month=1, day=1).timestamp())[0]
+        trades.append(tradeHistory)
+
+    tradeHistory = k.get_trades_history(start=datetime.datetime(year=today.year, month=1, day=1).timestamp(),
+                                        end=datetime.datetime.now().timestamp())[0]
+    trades.append(tradeHistory)
+    newTradeHistory = pd.concat(trades).reset_index()
+
     def removeNs(date):
         return str(date)[:-3]
     newTradeHistory['dtime'] = newTradeHistory['dtime'].apply(removeNs)
