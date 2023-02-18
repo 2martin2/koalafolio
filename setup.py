9234e3a2f5d6bb04a73e198fe19e50c16979d5e1
2martin2 <2martin2@protonmail.com>
2023-02-18 23:30:20 +0100

    version++

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', encoding="utf-8") as f:
 
 setuptools.setup(
     name='koalafolio',
-    version='0.11.13',
+    version='0.11.14',
     description='portfolio app for crypto trading and tax reporting',
     long_description=readme,
     long_description_content_type='text/x-rst',
