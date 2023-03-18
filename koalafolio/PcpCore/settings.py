6a9e1478c8d846152fa4c43ada3f875642a675f0
2martin2 <2martin2@protonmail.com>
2023-03-18 20:44:16 +0100

    version++

--- koalafolio/PcpCore/settings.py
+++ koalafolio/PcpCore/settings.py
@@ -15,7 +15,7 @@ import koalafolio.PcpCore.logger as logger
 
 dictRegex = re.compile(r'^\{ *(\'.+\' *\: *\'.+\' *\, *)* *\'.+\' *\: *\'.+\'\ *}$')
 
-VERSION = '0.11.16'
+VERSION = '0.11.17'
 
 class Settings(configparser.ConfigParser):
     def __init__(self, *args, **kwargs):
