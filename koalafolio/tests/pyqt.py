02ed454b0c380688a3866c8432906023baae64e5
2martin2 <gt-dev@m4n.slmail.me>
2025-06-01 01:33:26 +0200

    rework pyqt import

--- koalafolio/tests/pyqt.py
+++ koalafolio/tests/pyqt.py
@@ -1,15 +1,15 @@
-import PyQt5.QtGui as qtgui
-import PyQt5.QtWidgets as qtwidgets
-import PyQt5.QtCore as qtcore
+from PyQt5.QtGui import QFont, QPainter
+from PyQt5.QtWidgets import QApplication, QFileDialog, QFrame, QHBoxLayout, QHeaderView, QLabel, QLineEdit, QPushButton, QStackedLayout, QStyleFactory, QTableWidget, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt
 import PyQt5.QtChart as qtchart
 
 
 
-class MainWindow(qtwidgets.QWidget):
+class MainWindow(QWidget):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent=parent)
-        self.setStyle(qtwidgets.QStyleFactory.create('Fusion'))
-#        self.setWindowFlags(qtcore.Qt.FramelessWindowHint)
+        self.setStyle(QStyleFactory.create('Fusion'))
+#        self.setWindowFlags(Qt.FramelessWindowHint)
         self.setGeometry(500,300,1000,600)
         self.setWindowTitle("PyCryptoPortfolio")
 #        self.resize(1000,600)
@@ -23,52 +23,52 @@ class MainWindow(qtwidgets.QWidget):
 #        self.setObjectName("mainWindow")
 #        self.setStyleSheet("#mainWindow { background-color: white; border: 0px white}")
 
-        self.mainLayout = qtwidgets.QHBoxLayout(self)
+        self.mainLayout = QHBoxLayout(self)
         self.setContentsMargins(0,0,0,0)
  
         
-        self.sidebarFrame = qtwidgets.QFrame(self)
+        self.sidebarFrame = QFrame(self)
         self.sidebarFrame.setStyleSheet("background-color: red;")  
 #        self.sidebarFrame.setObjectName("sidebarFrame")
 #        self.sidebarFrame.setStyleSheet("#sidebarFrame { background-color: white; border: 0px white}")                        
-        self.sidebarFrame.setFrameShape(qtwidgets.QFrame.StyledPanel)
-        self.sidebarFrame.setFrameShadow(qtwidgets.QFrame.Raised)
+        self.sidebarFrame.setFrameShape(QFrame.StyledPanel)
+        self.sidebarFrame.setFrameShadow(QFrame.Raised)
         self.sidebarFrame.setFixedWidth(120)
         
-        self.contentFrame = qtwidgets.QFrame(self)
+        self.contentFrame = QFrame(self)
 #        self.contentFrame.setStyleSheet("background-color: yellow;")
 #        self.contentFrame.setObjectName("contentFrame")
 #        self.contentFrame.setStyleSheet("#contentFrame { background-color: white; border: 0px white}")
-        self.contentFrame.setFrameShape(qtwidgets.QFrame.StyledPanel)
-        self.contentFrame.setFrameShadow(qtwidgets.QFrame.Raised)
+        self.contentFrame.setFrameShape(QFrame.StyledPanel)
+        self.contentFrame.setFrameShadow(QFrame.Raised)
         
-        self.contentFrame2 = qtwidgets.QFrame(self)
+        self.contentFrame2 = QFrame(self)
 #        self.contentFrame.setStyleSheet("background-color: yellow;")
 #        self.contentFrame.setObjectName("contentFrame")
 #        self.contentFrame.setStyleSheet("#contentFrame { background-color: white; border: 0px white}")
-        self.contentFrame2.setFrameShape(qtwidgets.QFrame.StyledPanel)
-        self.contentFrame2.setFrameShadow(qtwidgets.QFrame.Raised)
+        self.contentFrame2.setFrameShape(QFrame.StyledPanel)
+        self.contentFrame2.setFrameShadow(QFrame.Raised)
         
         
         # stacked content layout
-        self.stackedLayout = qtwidgets.QStackedLayout()
+        self.stackedLayout = QStackedLayout()
         self.stackedLayout.addWidget(self.contentFrame)
         self.stackedLayout.addWidget(self.contentFrame2)
         
         
-        self.statusbarFrame = qtwidgets.QFrame(self)
+        self.statusbarFrame = QFrame(self)
         self.statusbarFrame.setStyleSheet("background-color: blue;")
 #        self.statusbarFrame.setObjectName("statusbarFrame")
 #        self.statusbarFrame.setStyleSheet("#statusbarFrame { background-color: white; border: 0px white}")
-        self.statusbarFrame.setFrameShape(qtwidgets.QFrame.StyledPanel)
-        self.statusbarFrame.setFrameShadow(qtwidgets.QFrame.Raised)
+        self.statusbarFrame.setFrameShape(QFrame.StyledPanel)
+        self.statusbarFrame.setFrameShadow(QFrame.Raised)
         self.statusbarFrame.setFixedHeight(30)
         
         # buttons sidebar
-        self.button1 = qtwidgets.QPushButton("b1", self.sidebarFrame)
-        self.button2 = qtwidgets.QPushButton("b2", self.sidebarFrame)
-        self.button3 = qtwidgets.QPushButton("b3", self.sidebarFrame)
-        self.button4 = qtwidgets.QPushButton("b4", self.sidebarFrame)
+        self.button1 = QPushButton("b1", self.sidebarFrame)
+        self.button2 = QPushButton("b2", self.sidebarFrame)
+        self.button3 = QPushButton("b3", self.sidebarFrame)
+        self.button4 = QPushButton("b4", self.sidebarFrame)
         buttonHeight = 80
         self.button1.setFixedHeight(buttonHeight)
         self.button2.setFixedHeight(buttonHeight)
@@ -78,7 +78,7 @@ class MainWindow(qtwidgets.QWidget):
         self.button2.clicked.connect(lambda: self.stackedLayout.setCurrentIndex(1))
 #        self.lowerBtn.setFixedSize(50,10)
         # layout in lower frame
-        self.sidebarVerticalLayout = qtwidgets.QVBoxLayout(self.sidebarFrame)
+        self.sidebarVerticalLayout = QVBoxLayout(self.sidebarFrame)
         self.sidebarVerticalLayout.addWidget(self.button1)
         self.sidebarVerticalLayout.addWidget(self.button2)
         self.sidebarVerticalLayout.addWidget(self.button3)
@@ -87,8 +87,8 @@ class MainWindow(qtwidgets.QWidget):
         
         # content contentFrame
         # content items
-        self.contentbutton = qtwidgets.QPushButton("some button", self.contentFrame)
-        self.contentlabel = qtwidgets.QLabel("some text", self.contentFrame)
+        self.contentbutton = QPushButton("some button", self.contentFrame)
+        self.contentlabel = QLabel("some text", self.contentFrame)
         mySeries = qtchart.QPieSeries()
         mySeries.append('BTC', 20000)
         mySeries.append('ETH', 60000)
@@ -100,15 +100,15 @@ class MainWindow(qtwidgets.QWidget):
 #        self.myChart.setTitle("chart example")
         self.myChart.legend().hide()
         self.myChartView = qtchart.QChartView(self.myChart)
-        self.myChartView.setRenderHint(qtgui.QPainter.Antialiasing)
+        self.myChartView.setRenderHint(QPainter.Antialiasing)
 #        self.contentFrame.addWidget(self.myChartView)
-        self.myTable = qtwidgets.QTableWidget(50,8,self.contentFrame)
-        self.myTable.horizontalHeader().setSectionResizeMode(qtwidgets.QHeaderView.Stretch)
+        self.myTable = QTableWidget(50,8,self.contentFrame)
+        self.myTable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
         # layout content
-        self.contentLayout = qtwidgets.QVBoxLayout(self.contentFrame)
-        self.horzLayout1 = qtwidgets.QHBoxLayout()
-        self.vertLayout1 = qtwidgets.QVBoxLayout()
+        self.contentLayout = QVBoxLayout(self.contentFrame)
+        self.horzLayout1 = QHBoxLayout()
+        self.vertLayout1 = QVBoxLayout()
         
         self.contentLayout.addLayout(self.horzLayout1)
         self.contentLayout.addWidget(self.myTable)
@@ -123,18 +123,18 @@ class MainWindow(qtwidgets.QWidget):
         # content contentFrame2
         # content items
         
-        self.pathlabel = qtwidgets.QLabel("Path:", self.contentFrame2)
-        self.fileDialog = qtwidgets.QFileDialog(self.contentFrame2)
-#        self.fileDialog.setFileMode(qtwidgets.QFileDialog.DirectoryOnly)
-        self.pathEntry = qtwidgets.QLineEdit(self.contentFrame2)
+        self.pathlabel = QLabel("Path:", self.contentFrame2)
+        self.fileDialog = QFileDialog(self.contentFrame2)
+#        self.fileDialog.setFileMode(QFileDialog.DirectoryOnly)
+        self.pathEntry = QLineEdit(self.contentFrame2)
         self.pathEntry.setPlaceholderText("enter Path/ drop file or folder")
-        self.selectPathButton = qtwidgets.QPushButton("Select", self.contentFrame2)
+        self.selectPathButton = QPushButton("Select", self.contentFrame2)
         self.selectPathButton.clicked.connect(lambda: self.selectPath())
        
 
         # layout content
-        self.contentLayout2 = qtwidgets.QVBoxLayout(self.contentFrame2)
-        self.horzLayout21 = qtwidgets.QHBoxLayout()
+        self.contentLayout2 = QVBoxLayout(self.contentFrame2)
+        self.horzLayout21 = QHBoxLayout()
         
         self.contentLayout2.addLayout(self.horzLayout21)
         self.horzLayout21.addWidget(self.pathlabel)
@@ -143,14 +143,14 @@ class MainWindow(qtwidgets.QWidget):
 
         
         # labels statusbar
-        self.label1 = qtwidgets.QLabel("little bit status", self.statusbarFrame)
-        font = qtgui.QFont("Arial", 10)
+        self.label1 = QLabel("little bit status", self.statusbarFrame)
+        font = QFont("Arial", 10)
         self.label1.setFont(font)
-        self.label2 = qtwidgets.QLabel("some more status", self.statusbarFrame)
+        self.label2 = QLabel("some more status", self.statusbarFrame)
         self.label2.setFont(font)
 #        self.lowerBtn.setFixedSize(50,10)
         # layout in lower frame
-        self.statusbarHorizontalLayout = qtwidgets.QHBoxLayout(self.statusbarFrame)
+        self.statusbarHorizontalLayout = QHBoxLayout(self.statusbarFrame)
         self.statusbarHorizontalLayout.addWidget(self.label1)
         self.statusbarHorizontalLayout.addWidget(self.label2)
         self.statusbarHorizontalLayout.addStretch()
@@ -158,13 +158,13 @@ class MainWindow(qtwidgets.QWidget):
         
         
         # put frames in horizontal layout
-        self.horizontalFrameLayout = qtwidgets.QHBoxLayout()
+        self.horizontalFrameLayout = QHBoxLayout()
         self.horizontalFrameLayout.addWidget(self.sidebarFrame)
         self.horizontalFrameLayout.addLayout(self.stackedLayout)
 
         
         # put frames in vertical layout
-        self.verticalFrameLayout = qtwidgets.QVBoxLayout()
+        self.verticalFrameLayout = QVBoxLayout()
         self.verticalFrameLayout.setSpacing(0)
         self.verticalFrameLayout.addLayout(self.horizontalFrameLayout)
         self.verticalFrameLayout.addWidget(self.statusbarFrame)
@@ -186,7 +186,7 @@ class MainWindow(qtwidgets.QWidget):
         
 
 if __name__ == '__main__':
-    app = qtwidgets.QApplication([])
-#    app.setStyle(qtwidgets.QStyleFactory.create('Fusion'))
+    app = QApplication([])
+#    app.setStyle(QStyleFactory.create('Fusion'))
     window = MainWindow()
     app.exec_()
